59b92bf80b4963af5ffc5cd62070006cb4e9f572
wbzhao <wbzhao7@gmail.com>
2016-06-07 10:11:47 +0800

    created 20160607

---
+++ src/com/derun/drclient/DrClient.java
@@ -0,0 +1,32 @@
+package com.derun.drclient;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+
+public class DrClient extends JWindow {
+	
+	private static final long serialVersionUID = 1L;
+
+	public DrClient(String filename, Frame f) 
+    { 
+        super(f); 
+        JLabel l = new JLabel(new ImageIcon(filename)); 
+        getContentPane().add(l, BorderLayout.CENTER); 
+        pack(); 
+        Dimension screenSize = 
+          Toolkit.getDefaultToolkit().getScreenSize(); 
+        Dimension labelSize = l.getPreferredSize(); 
+        setLocation(screenSize.width/2 - (labelSize.width/2), 
+                    screenSize.height/2 - (labelSize.height/2)); 
+        setVisible(true); 
+        screenSize = null; 
+        labelSize = null; 
+    } 
+
+}
---
+++ src/com/derun/log/LogUtil.java
@@ -0,0 +1,52 @@
+package com.derun.log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class LogUtil {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String logFilePath = "D:\\ccslog\\201601\\ccslog.2016-01-04";
+		//String logFilePath = "C:\\Users\\Administrator\\Desktop\\ccslog.2016-04-26";
+		scanLog(logFilePath);
+	}
+	
+	public static void scanLog(String logFilePath) {
+		try {
+			int maxCost = 0;
+			String maxLine = "";
+			String encoding = "UTF8";
+			File file = new File(logFilePath);
+			if (file.isFile() && file.exists()) { // 判断文件是否存在
+				InputStreamReader read = new InputStreamReader(
+				new FileInputStream(file), encoding);// 考虑到编码格式
+				BufferedReader bufferedReader = new BufferedReader(read);
+				String lineTxt = null;
+				while ((lineTxt = bufferedReader.readLine()) != null) {
+					if(lineTxt.contains("共计耗时：【")){
+						int beginIndex = lineTxt.indexOf("共计耗时：【");
+						int endIndex = lineTxt.indexOf("】 毫秒");
+						String cost = lineTxt.substring(beginIndex+6, endIndex);
+						int thisCost = Integer.parseInt(cost);
+						if(thisCost>maxCost){
+							maxCost = thisCost;
+							maxLine = lineTxt;
+						}
+					}
+				}
+				System.out.println("最长耗时：");
+				System.out.println(maxLine);
+				read.close();
+			} else {
+				System.out.println("找不到指定的文件");
+			}
+		} catch (Exception e) {
+			System.out.println("读取文件内容出错");
+			e.printStackTrace();
+		}
+	}
+
+}
